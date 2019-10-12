b5693bc3b9e7ad29690e7a5c655a416a8a6c12a0
A-Wadhwani <wadhwani@purdue.edu>
2019-10-12 09:40:22 -0400

    Added Google Cloud Stuff

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Google.Cloud.Vision.V1;
+using Image = Google.Cloud.Vision.V1.Image;
+using Page = System.Web.UI.Page;
 
 namespace Hack4HongKong
 {
@@ -11,7 +14,15 @@ namespace Hack4HongKong
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string filePath = @"C:\Users\Aryan Wadhwani\source\repos\Hack4HongKong\TestImages\blizzard.webp";
+            var image = Image.FromFile(filePath);
+            var client = ImageAnnotatorClient.Create();
+            var response = client.DetectLogos(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
         }
     }
 }
