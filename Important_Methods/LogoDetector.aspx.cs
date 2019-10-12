d89ff0dc71dca86f762f91adaa7b6a8166f68c7a
Kade Boltjes <KadeCode15@gmail.com>
2019-10-12 16:24:21 -0400

    here it is pushed

--- Important_Methods/LogoDetector.aspx.cs
+++ Important_Methods/LogoDetector.aspx.cs
@@ -17,44 +17,52 @@ namespace Hack4HongKong.Important_Methods
         protected void Unnamed_Click(object sender, EventArgs e)
         {
             string path = MapPath(@"~/LogoImages/image.jpg");
-            fileup.PostedFile.SaveAs(path);
-            string fileName = fileup.PostedFile.FileName;
-            var allCompaniesWithinPicture = Important_Methods.CheckforLogo.CompaniesFoundInPicture(path);
-
-
-            txtValueA.Text = "";
-            for (int x = 0; x < allCompaniesWithinPicture.Length; x++)
+            if(fileup.PostedFile.ContentLength > 0)
             {
-                /*TableRow tr = new TableRow();
 
-                TableCell nameCell = new TableCell();
-                nameCell.Text = allCompanies[x].CompanyName;
-                tr.Cells.Add(nameCell);
-
-
-                tr.Cells.Add(new TableCell());
-
-                TableCell descCell = new TableCell();
-                descCell.Text = allCompanies[x].Description;
-                tr.Cells.Add(descCell);*/
+                fileup.PostedFile.SaveAs(path);
+                string fileName = fileup.PostedFile.FileName;
+                var allCompaniesWithinPicture = Important_Methods.CheckforLogo.CompaniesFoundInPicture(path);
 
 
+                txtValueA.Text = "";
+                for (int x = 0; x < allCompaniesWithinPicture.Length; x++)
+                {
+                    /*TableRow tr = new TableRow();
 
-                //protected string companyName = allCompanies[x].CompanyName;
-                txtValueA.Text +=
-                   "<div class=\"panel panel-primary\">" +
-                       "<div class=\"panel-heading\">" +
-                           " <b>" + allCompaniesWithinPicture[x].CompanyName + "</b> " +
-                       "</div>" +
-                       "<div class=\"panel-body\">" +
-                           " " + allCompaniesWithinPicture[x].Description + " " +
-                        "</div>" +
-                    "</div>";
+                    TableCell nameCell = new TableCell();
+                    nameCell.Text = allCompanies[x].CompanyName;
+                    tr.Cells.Add(nameCell);
+
+
+                    tr.Cells.Add(new TableCell());
+
+                    TableCell descCell = new TableCell();
+                    descCell.Text = allCompanies[x].Description;
+                    tr.Cells.Add(descCell);*/
 
 
 
-                // mainTable.Rows.Add(tr);
-                //companyList.Items.Add(allCompanies[x].CompanyName);
+                    //protected string companyName = allCompanies[x].CompanyName;
+                    txtValueA.Text +=
+                       "<div class=\"panel panel-primary\">" +
+                           "<div class=\"panel-heading\">" +
+                               " <b>" + allCompaniesWithinPicture[x].CompanyName + "</b> " +
+                           "</div>" +
+                           "<div class=\"panel-body\">" +
+                               " " + allCompaniesWithinPicture[x].Description + " " +
+                            "</div>" +
+                        "</div>";
+
+
+
+                    // mainTable.Rows.Add(tr);
+                    //companyList.Items.Add(allCompanies[x].CompanyName);
+                }
+            }
+            else
+            {
+
             }
         }
     }
