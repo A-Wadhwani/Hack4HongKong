4a41dd0088244e2a9eb2a392a1713869e79b49f9
A-Wadhwani <wadhwani@purdue.edu>
2019-10-12 12:29:21 -0400

    Added Webcam

---
+++ Important_Methods/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hack4HongKong.Important_Methods
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void snap_Click(object sender, EventArgs e)
+        {
+            String s = myimage.Src;
+            
+        }
+    }
+}
