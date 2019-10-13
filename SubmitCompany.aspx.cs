662fbdc77084b21d26d3b77c12c1cabfd43bfdb6
Kade Boltjes <KadeCode15@gmail.com>
2019-10-12 22:06:40 -0400

    added event.html

--- SubmitCompany.aspx.cs
+++ SubmitCompany.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +13,7 @@ namespace Hack4HongKong
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SendMail("kade", "Google", "IT WORKS");
             //AddCompany("Facebook", "Other Description", new string[] { "tag1", "tag2" });
         }
         private void AddCompany(string companyName, string description, string[] tags)
@@ -39,6 +42,36 @@ namespace Hack4HongKong
             AddCompany(companyNameTxtBox.Text, descTextBox.Text, null);
             companyNameTxtBox.Text = String.Empty;
             descTextBox.Text = String.Empty;
+        }
+
+        private void SendMail(string recipientName, string company, string desc)
+        {
+            string filename = Server.MapPath("~/EmailVerification/Event.html");
+            string mailbody = System.IO.File.ReadAllText(filename);
+            mailbody = mailbody.Replace("##RecipientName##", recipientName);
+            mailbody = mailbody.Replace("##Company##", company);
+            mailbody = mailbody.Replace("##Description##", desc);
+
+
+            string to = "kab@gmalia.com";
+            string from = "YouEmailID@gmail.com";
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Auto Response Email";
+            message.Body = mailbody;
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            System.Net.NetworkCredential basicCredential = new System.Net.NetworkCredential("YouEmailID@gmail.com", "Password");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = true;
+            client.Credentials = basicCredential;
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
         }
     }
 }
