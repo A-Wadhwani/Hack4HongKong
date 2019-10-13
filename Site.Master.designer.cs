8f6b6030c3fccce2346583663be10b1707a691db
Kade Boltjes <KadeCode15@gmail.com>
2019-10-12 20:48:39 -0400

    garbage commits

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -10,7 +10,16 @@
 namespace Hack4HongKong {
     
     
-    public partial class SiteMaster {
+    public partial class Site {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
         /// MainContent control.
